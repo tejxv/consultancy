9c550520e5425bfd57eed30439b7451038a3cd61
vishallsinghh <vishalsingh173911@gmail.com>
2021-08-05 16:59:04 +0530

    new commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -28,7 +28,7 @@ const Header=()=> {
                 </div>
             </div>
             <div className='section2'>
-                ------Navbar------
+                ------Navbar-------
             </div>
         </div>
     )
